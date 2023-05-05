fc3866e524a88215fdd8dff3bca7e5471fc55931
OctOcat <willierivas97@gmail.com>
2023-05-05 02:43:02 -0500

    NewestVersion

--- src/test/java/TransactionManager.java
+++ src/test/java/TransactionManager.java
@@ -2,23 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionManager {
+    // List to hold all the transactions
     private final List<Transaction> transactions;
 
+    // Constructor to initialize the transactions list
     public TransactionManager() {
         this.transactions = new ArrayList<>();
     }
 
+    // Method to add a transaction to the transactions list
     public void addTransaction(Transaction transaction) {
         this.transactions.add(transaction);
     }
 
+    // Method to display the ledger of all transactions
     public void displayLedger() {
         System.out.println("Date       Time       Description            Vendor                Amount");
         System.out.println("----------------------------------------------------------------------------");
 
+        // Loop through all the transactions and print each one
         for (Transaction transaction : this.transactions) {
             System.out.println(transaction.toString());
         }
+
 
         System.out.println("----------------------------------------------------------------------------");
     }
